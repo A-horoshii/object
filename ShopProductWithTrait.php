51c7d6826a2cdf509fc8b9f00313b5b55abe9647
horoshii <khoroshko.andrey@gmail.com>
2017-01-09 12:27:22 +0200

    trait example

--- ShopProductWithTrait.php
+++ ShopProductWithTrait.php
@@ -5,15 +5,22 @@
  * Date: 28.12.2016
  * Time: 12:54
  */
-trait PriceUtilities
-{
-    private $tax_rate = 17;
+//trait PriceUtilities
+//{
+//    private $tax_rate = 17;
+//
+//    function calculateTax($price){
+//        return (($this->tax_rate/100)*$price);
+//    }
+//}
+
+trait PriceUtilities{
 
     function calculateTax($price){
-        return (($this->tax_rate/100)*$price);
+        return(($this->getTaxRate()/100)*$price);
     }
+    abstract function getTaxRate();
 }
-
 trait TaxTools{
     function calculateTax($price){
         return 222;
@@ -21,6 +28,10 @@ trait TaxTools{
 }
 class ShopProduct{
     use PriceUtilities;
+    function getTaxRate()
+    {
+        return 12;
+    }
 }
 
 abstract class Service{
@@ -30,7 +41,13 @@ abstract class Service{
 class UtilityService extends Service {
     use PriceUtilities, TaxTools{
         TaxTools::calculateTax insteadof PriceUtilities;
+        PriceUtilities::calculateTax as basicTax;
+    }
+    function getTaxRate()
+    {
+      return 17;
     }
 }
 $p = new UtilityService();
-print $p->calculateTax(123);
+//print $p->calculateTax(123);
+print $p->basicTax(1223);
